d4aaad36528b5e069bebce1f4d31bf757be9d73d
Max Bonhomme <hello.bonhomme@gmail.com>
2022-08-30 20:17:35 +0200

    add goerli

--- packages/frontend/config.ts
+++ packages/frontend/config.ts
@@ -1,2 +1,8 @@
-export const NETWORK_ID = 31337 as number;
+const chains: { [name: string]: number } = {
+  development: 31337,
+  preview: 5,
+  production: 1
+}
+
+export const NETWORK_ID = chains[process.env.NODE_ENV as string];
 export const NETWORK_NAME = 'localhost' as string;

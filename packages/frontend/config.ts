0b5e814de9dbbd612b15db5f877302055d99d0ba
Max Bonhomme <hello.bonhomme@gmail.com>
2022-08-30 22:14:18 +0200

    goerli

--- packages/frontend/config.ts
+++ packages/frontend/config.ts
@@ -1,8 +1,8 @@
-const chains: { [name: string]: number } = {
-  development: 31337,
-  preview: 5,
-  production: 1
-}
+// const chains: { [name: string]: number } = {
+//   development: 31337,
+//   preview: 5,
+//   production: 1
+// }
 
-export const NETWORK_ID = chains[process.env.NODE_ENV as string];
+export const NETWORK_ID = 5;
 export const NETWORK_NAME = 'localhost' as string;

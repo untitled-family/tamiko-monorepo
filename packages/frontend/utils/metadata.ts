27c14972313b73e0e8f89fd119b0540a63b99445
Max Bonhomme <hello.bonhomme@gmail.com>
2022-10-12 22:35:20 +0100

    fix types

--- packages/frontend/utils/metadata.ts
+++ packages/frontend/utils/metadata.ts
@@ -1,20 +1,22 @@
-import { Property, Attribute } from "@/hooks/useTamikoMetadata";
-import { Item } from "@/hooks/useTamikoStoreItems";
-import { BigNumber } from "ethers";
+import { Attribute, TamikoProperties, TamikoStoreItem } from "@/types/metadata"
+import { BigNumber } from "ethers"
 
 /**
  * Filter an array of attributes by traits
  * @param attributes array of all attributes to look through
  * @param traits array of traits to be extracted
- * @returns Array of Attributes 
+ * @returns Array of Attributes
  */
-export const extractAttributes = (attributes: Attribute[], traits: string[]): Attribute[] => {
+export const extractAttributes = (
+  attributes: Attribute[],
+  traits: string[]
+): Attribute[] => {
   if (!attributes) return []
 
-  const attrs = attributes.map(attr => traits.includes(attr.trait_type) && attr);
-  const filtered = attrs.filter(Boolean) as Attribute[];
+  const attrs = attributes.map((attr) => traits.includes(attr.trait_type) && attr)
+  const filtered = attrs.filter(Boolean) as Attribute[]
 
-  return filtered;
+  return filtered
 }
 
 /**
@@ -22,10 +24,13 @@ export const extractAttributes = (attributes: Attribute[], traits: string[]): At
  * @param properties array of all attributes to convert to object
  * @returns Object of Property
  */
-export const attributesToObject = (properties: Attribute[]): Property => {
-  const propertiesObject = properties.reduce((obj, item) => Object.assign(obj, { [item.trait_type]: item.value }), {}) as Property
+export const attributesToObject = (properties: Attribute[]): TamikoProperties => {
+  const propertiesObject = properties.reduce(
+    (obj, item) => Object.assign(obj, { [item.trait_type]: item.value }),
+    {}
+  ) as TamikoProperties
 
-  return propertiesObject;
+  return propertiesObject
 }
 
 /**
@@ -35,17 +40,17 @@ export const attributesToObject = (properties: Attribute[]): Property => {
  */
 type ItemTypes = number | string | BigNumber
 
-export const extractItemProperties = (array: ItemTypes[]): Item => {
-  const keys = ['id', 'name', 'description', 'svg', 'price', 'creator']
-  const bigNumbers = ['id', 'price']
+export const extractItemProperties = (array: ItemTypes[]): TamikoStoreItem => {
+  const keys = ["id", "name", "description", "svg", "price", "creator"]
+  const bigNumbers = ["id", "price"]
 
   const item = {
     id: 0,
-    name: '',
-    description: '',
-    svg: '',
+    name: "",
+    description: "",
+    svg: "",
     price: 0,
-    creator: '0x0'
+    creator: "0x0",
   }
 
   keys.forEach((key, index) => {
